046476c7c405e27eb970d985aba016ea780f76b6
David Conner <dconner.pro@gmail.com>
2013-05-14 02:42:34 -0600

    rails new incense -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Incense::Application.config.secret_key_base = '91ca23af32c5990191484434014c34fbc9145e41611adbec0ef6e0849e8c0f18311f81a6f1b2d50436bb92d1f829d310931f8b32981c285a7d995c4bb3faa149'
