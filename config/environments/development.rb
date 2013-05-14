265971d5c7c52f7bc1c5fd9f6aa08ed0cc8f46d8
David Conner <dconner.pro@gmail.com>
2013-05-14 02:53:17 -0600

    rails dev environment: added ember dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Incense::Application.configure do
+  config.ember.variant = :development     #ember dev mode
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
